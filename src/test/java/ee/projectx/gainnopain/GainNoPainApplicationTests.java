2bfb276348442fb42ada51068e5a5cec0a8587a2
Sergey Domnin <sergeydomninit@gmail.com>
2023-08-08 11:17:19 +0300

    First commit

---
+++ src/test/java/ee/projectx/gainnopain/GainNoPainApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.projectx.gainnopain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GainNoPainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
